4a19962594c95dee843c228ab86c032851129486
aparg <apargtm@gmail.com>
2024-04-15 18:11:08 +0545

    trial for embedding

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import React from "react";
 export default async function Home() {
   return (
     <iframe
-      src="http://localhost:3000/embedded-site/ontario"
+      src="https://dolphy-commercial-two.vercel.app/embedded-site/ontario"
       style={{ width: "100%", height: "100vh" }}
     ></iframe>
   );
