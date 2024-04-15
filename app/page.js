6098259e40a4c721d40330085b8fc3c2d029489c
aparg <apargtm@gmail.com>
2024-04-15 18:19:12 +0545

    fix: style fix

--- app/page.js
+++ app/page.js
@@ -4,8 +4,8 @@ export default async function Home() {
   return (
     <iframe
       src="https://dolphy-commercial-two.vercel.app/embedded-site/ontario"
-      style={{ width: "100%" }}
-      scrolling="no"
+      style={{ width: "100%", height: "95vh", overflow: "hidden" }}
+      scrolling="yes"
     ></iframe>
   );
 }
