3995833b46841580029587939a795b11963f4802
aparg <apargtm@gmail.com>
2024-04-15 18:13:50 +0545

    trial for embedding

--- app/page.js
+++ app/page.js
@@ -4,7 +4,8 @@ export default async function Home() {
   return (
     <iframe
       src="https://dolphy-commercial-two.vercel.app/embedded-site/ontario"
-      style={{ width: "100%", height: "100vh" }}
+      style={{ width: "100" }}
+      scrolling="no"
     ></iframe>
   );
 }
